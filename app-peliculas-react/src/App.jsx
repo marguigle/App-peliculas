7a81d0a01c31ef427ef95948d283ef6d7c4f5a18
Marcelo G Iglesia <marguigle@yahoo.com.ar>
2024-03-17 17:22:20 -0300

    se crea filtro por nombre

--- app-peliculas-react/src/App.jsx
+++ app-peliculas-react/src/App.jsx
@@ -1,11 +1,21 @@
 import "./App.css";
 import CardMovies from "./components/CardMovies";
+import { useState } from "react";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+  function handleSearchMovie(event) {
+    console.log(event.target.value);
+    setSearchTerm(event.target.value);
+  }
   return (
     <>
       <h1>APP de peliculas</h1>
-      <CardMovies />
+      <div className="search-container">
+        <input type="text" value={searchTerm} onChange={handleSearchMovie} />
+        <button>search</button>
+      </div>
+      <CardMovies searchTerm={searchTerm} />
     </>
   );
 }
--- app-peliculas-react/src/components/CardMovies.jsx
+++ app-peliculas-react/src/components/CardMovies.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import "./CardMovies.css";
+import PropTypes from "prop-types";
 
 const apiKey = "c268ca13195bf833f84d0060308f7cd3";
 const endpoint = "https://api.themoviedb.org/3/discover/movie";
@@ -7,44 +8,30 @@ const params = {
   api_key: apiKey,
   page: 2,
 };
-const url = new URL(endpoint);
-url.search = new URLSearchParams(params).toString();
 
-const CardMovies = () => {
+const CardMovies = ({ searchTerm }) => {
   const [peliculas, setPeliculas] = useState([]);
 
   useEffect(() => {
+    const url = new URL(endpoint);
+    url.search = new URLSearchParams(params).toString();
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
         setPeliculas(data.results);
       })
       .catch((error) => console.error("Error:", error));
-  }, []);
+  }, [searchTerm]);
   console.log(peliculas);
-  // return (
-
-  //     <div className="card-movies">
-  //       {peliculas.map((pelicula) => (
-  //         <div key={pelicula.id} className="cardMovies">
-  //           <h3>{pelicula.original_title}</h3>
-  //           <p>Resumen: {pelicula.overview}</p>
-  //           <img
-  //             src={`https://image.tmdb.org/t/p/w500${pelicula.poster_path}`}
-  //             alt="Poster"
-  //           />
-  //           <p>Popularidad: {pelicula.popularity}</p>
-  //           <p>Lenguaje original: {pelicula.original_language}</p>
-  //           <p>Fecha de lanzamiento: {pelicula.release_date}</p>
-  //         </div>
-  //       ))}
-  //     </div>
-
-  // );
+  const peliculasFiltradas = peliculas.filter(
+    (pelicula) =>
+      pelicula.original_title &&
+      pelicula.original_title.toLowerCase().startsWith(searchTerm.toLowerCase())
+  );
 
   return (
     <div>
-      {peliculas.map((pelicula) => (
+      {peliculasFiltradas.map((pelicula) => (
         <div key={pelicula.id} className="card-movies ">
           <h3>{pelicula.original_title}</h3>
           <p>Resumen: {pelicula.overview}</p>
@@ -59,6 +46,9 @@ const CardMovies = () => {
       ))}
     </div>
   );
+};
+CardMovies.propTypes = {
+  searchTerm: PropTypes.string.isRequired, // Valida el tipo de la prop searchTerm
 };
 
 export default CardMovies;
